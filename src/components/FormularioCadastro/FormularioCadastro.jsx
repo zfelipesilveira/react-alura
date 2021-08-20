dd1bb5173ede9fd59df4fe657266bd3cb951c59f
zfelipesilveira <zfelipesilveira@gmail.com>
2021-08-20 19:41:11 -0300

    projeto de app de notas da alura completo

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,37 +1,60 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
+  constructor(props) {
+    super(props);
+    this.titulo = "";
+    this.texto = "";
+    this.categoria = "Sem Categoria";
+    this.state = {categorias:[]}
+
+    this._novasCategorias = this._novasCategorias.bind(this);
+  }
+
+  componentDidMount(){
+    this.props.categorias.inscrever( this._novasCategorias);
     
-    constructor(props){
-        super(props);
-        this.titulo="";
-        this.texto="";
-    }
-    
-    _handleMudancaTitulo(evento){
-        this.titulo = evento.target.value;
-        evento.stopPropagation();
-    }
+  }
 
-    _handleMudancaTexto(evento){
-        this.texto = evento.target.value;
-        evento.stopPropagation();
-    }
+  componentWillUnmount(){
+    this.props.categorias.desinscrever( this._novasCategorias);
+  }
+  _novasCategorias(categorias){
+    this.setState({...this.state, categorias})
+  }
+  _handleMudancaCategoria(evento){
+    evento.stopPropagation();
+    this.categoria = evento.target.value;
+  }
+  _handleMudancaTitulo(evento) {
+    evento.stopPropagation();
+    this.titulo = evento.target.value;
+  }
 
-    _criarNota(evento){
-        evento.preventDefault();
-        evento.stopPropagation();
-        this.props.criarNota(this.titulo, this.texto);
+  _handleMudancaTexto(evento) {
+    evento.stopPropagation();
+    this.texto = evento.target.value;
+  }
 
+  _criarNota(evento) {
+    evento.preventDefault();
+    evento.stopPropagation();
+    this.props.criarNota(this.titulo, this.texto, this.categoria);
+  }
 
-    }
-  
-  
-    render() {
+  render() {
     return (
-      <form className="form-cadastro"
-        onSubmit={this._criarNota.bind(this)} 
-      >
+      <form className="form-cadastro" onSubmit={this._criarNota.bind(this)}>
+        <select
+          onChange={this._handleMudancaCategoria.bind(this)}
+          className="form-cadastro_input"
+        >
+          <option>Sem Categoria</option>
+
+          {this.state.categorias.map((categoria, index) => {
+            return <option key={index} >{categoria}</option>;
+          })}
+        </select>
         <input
           type="text"
           placeholder="Título"
